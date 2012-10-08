d163d3b57bbf08d84cfdb6750e3a367cba713791
Anuj Jamwal <anujjamwal@thoughtworks.com>
2012-10-08 18:48:11 +0530

    Initial Commit

---
+++ ezOMR/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ezOMR
+//
+//  Created by Anuj Jamwal on 08/10/12.
+//  Copyright (c) 2012 Anuj Jamwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
